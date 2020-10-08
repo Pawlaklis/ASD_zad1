50a38759b8633f5b4351fb2ad169d25effe3921b
Paweł Knapczyk <Pawlak.lis@gmail.com>
2020-10-08 17:26:06 +0200

    Adding print function and random array generator to Array_class.

--- Array_class.h
+++ Array_class.h
@@ -1,10 +1,21 @@
+#include <ctime>
+
 class ARRAY_CLASS {
     int size;
     int *array;
+public:
+    ARRAY_CLASS();
     ARRAY_CLASS(int size, int array[]);
     ~ARRAY_CLASS();
     void insertion_sort();
+    void generate_random(int);
+    void print();
 };
+
+ARRAY_CLASS::ARRAY_CLASS() {
+    size = 0;
+    array = NULL;
+}
 
 ARRAY_CLASS::ARRAY_CLASS(int size, int *array) {
     this->size = size;
@@ -28,5 +39,21 @@ void ARRAY_CLASS::insertion_sort() {
         }
         array[i + 1] = element;
     }
+}
+
+void ARRAY_CLASS::generate_random(int n) {
+    size = n;
+    array = new int[size];
+    srand(time(NULL));
+    for (int i = 0; i < size; ++i) {
+        array[i] = rand()%1000-500;
+    }
+}
+
+void ARRAY_CLASS::print() {
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
 }
 
